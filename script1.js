62a08040902b2ed6d15e4108cc8cf918724b150b
harshanand2021 <harsh250600@gmail.com>
2023-08-23 22:32:28 +0530

    initial commit

---
+++ script1.js
@@ -0,0 +1,16 @@
+// currySum(1,2,3)(4,5,6)(7,8,9)
+let sum = 0;
+function currySum(...args) {
+  args.forEach((item) => {
+    sum += item;
+  });
+  return (...b) => {
+    if (b.length === 0) {
+        return sum;
+    } else {
+        return currySum(...b);
+    }
+  };
+}
+
+console.log(currySum(1, 2, 3, 4)(5, 6, 7)(8, 9, 10)(11, 12, 13, 15)());
